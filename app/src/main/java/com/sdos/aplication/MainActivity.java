09b23cdc97a6f5f8b1253d723049ca975874685f
Fernando Piñeiro <fernandomanuelp90@gmail.com>
2021-06-30 20:16:14 +0200

    Iniciamos proyecto

---
+++ app/src/main/java/com/sdos/aplication/MainActivity.java
@@ -0,0 +1,15 @@
+package com.sdos.aplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getResources().get
+        setContentView(R.layout.activity_main);
+    }
+}
