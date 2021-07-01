966beaa1c69195a21132cb42b67e05763bbd76af
Fernando Piñeiro <fernandomanuelp90@gmail.com>
2021-07-01 22:05:53 +0200

    añade de forma manual los datos del modelo

---
+++ app/src/main/java/com/sdos/aplication/Model/ModeloBD.java
@@ -0,0 +1,39 @@
+package com.sdos.aplication.Model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ModeloBD {
+
+
+    private String[] listaCodigo(){
+        return new String[] {
+                "10","12","13","14","15","16","17","18",
+        };
+    }
+
+    private String[] listaNombre(){
+        return new String[] {
+               "Laura","Fernando","Pablo","Poseidon","Irina","Helena","Atenea","Artemisa"
+        };
+    }
+
+    private String[] listaTelefono(){
+        return new String[] {
+                "652525252","659848785","689456320","653589574","659874123","658784521","652541236","689847411"
+        };
+    }
+
+    private String[] listaEmail(){
+        return new String[] {
+                "example@gmail.com"
+        };
+    }
+
+    private String[] listaVisitado(){
+        return new String[] {
+                "1","1","1","0","1","0","0","0"
+        };
+    }
+
+}
